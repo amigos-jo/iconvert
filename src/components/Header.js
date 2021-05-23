ec56ed467710dc0ef6cc38d90b04bbe31971c30d
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-23 22:16:15 +0300

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+import Logo from '../assets/logo.png';
 
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar bg="primary"  >
-          <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar className='headerColor'  >
+          <img src={Logo} alt='logo' className='logo' />
+
           <Nav className="mr-auto items">
             <Nav.Link className="item" href="#home">Home</Nav.Link>
             <Nav.Link className="item" href="#features">About us</Nav.Link>
