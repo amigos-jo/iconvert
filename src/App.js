0c8d307a94f485cf1be0f7e5237f921d2db8e854
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-05-23 22:20:24 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import HomeBack from './components/HomeBack';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExchangeForm from './components/ExchangeForm';
+import Chart from './components/Chart';
+import MyChart from './components/Chart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <HomeBack />
+        <ExchangeForm />
+        <MyChart/>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
